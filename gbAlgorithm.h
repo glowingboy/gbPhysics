ebb8bf8db8da1668461acc18a317e74bc9fcc23e
glowingboy <glowingboy@outlook.com>
2017-11-08 21:47:08 +0800

    quickSelectMost bug fixed

--- gbAlgorithm.h
+++ gbAlgorithm.h
@@ -5,6 +5,18 @@ namespace gb
   namespace Algorithm
   {
       /*
+       *@brief default compare function object type for basic type
+       */
+      template<typename DataType>
+      struct DefaultCompare
+      {
+	  bool operator()(const DataType& l, const DataType& r)
+	      {
+		  return l <= r;
+	      }
+      };
+      
+      /*
        *@brief quick select most n large/small, and store them in origin array from idx:0 to idx:n
        *@tparam Compare a function object type, 
        *possible like:
@@ -12,9 +24,11 @@ namespace gb
        *@tparam Swap exchange two elements' position
        *@param data input array
        *@param count how many elements you want to select
+       *@warning Data assignment operator will be used to 
+       *assign value to pivot variable
        */
-      template<typename Data, typename Compare>
-      static void quickSelectMost(Data* data, const std::uint32_t leftIdx, const std::uint32_t rightIdx, const std::uint32_t count)
+	  template<typename DataType, typename Compare = DefaultCompare<DataType>>
+      static void quickSelectMost(DataType* data, const std::uint32_t leftIdx, const std::uint32_t rightIdx, const std::uint32_t count)
       {
 	  if(leftIdx >= rightIdx)
 	      return;
@@ -22,7 +36,7 @@ namespace gb
 	      return;
 	  Compare cmpr;
 	  std::uint32_t pivotIdx = leftIdx;
-	  Data pivot = data[pivotIdx];
+	  DataType pivot = data[pivotIdx];
 	  
 	  std::uint32_t i = leftIdx;
 	  std::uint32_t j = rightIdx;
@@ -56,12 +70,16 @@ namespace gb
 
 	  const std::uint32_t curCount = i - leftIdx + 1;
 	  
-	  if( curCount >= count)
-	      return;
+	  if( curCount < count)
+	      quickSelectMost<DataType, Compare>(data, i + 1, rightIdx, count - curCount);
 	  else
 	  {
-	      quickSelectMost<Data, Compare>(data, i + 1, rightIdx, count - curCount);
+	      quickSelectMost<DataType, Compare>(data, leftIdx, i, count);
 	  }
       }
+
+      template<typename DataType, typename Compare>
+      void midian();
+      
   }
-};
+}
--- phytest/test.cpp
+++ phytest/test.cpp
@@ -26,6 +26,7 @@ int main(int argc, char** argv)
 {
     const unsigned int count = 100;
     testData data[count];
+    int intArray[count];
     std::cout<< std::endl << "********************************" << std::endl;
     for(int i = 0; i < count; i++)
     {
@@ -34,14 +35,37 @@ int main(int argc, char** argv)
     }
 
     std::cout<< std::endl << "********************************" << std::endl;
-    
-    gb::Algorithm::quickSelectMost<testData, testData::compare>(data, 0, 99, 10);
+
+    const unsigned int mostCount = 50;
+    gb::Algorithm::quickSelectMost<testData, testData::compare>(data, 0, 99, mostCount);
 //    gb::Algorithm::quickSelectMost(data, 0, 100, 10);
     for(int i = 0 ; i < count; i++)
     {
 	std::cout << data[i].key << ", ";
+	if( i == (mostCount -1))
+	    std::cout << "****, ";
     }
     
     std::cout<< std::endl << "********************************" << std::endl;
+
+
+    for(int i = 0; i < count; i++)
+    {
+	intArray[i] = rand()%100;
+	std::cout << intArray[i] << ", ";
+    }
+    std::cout<< std::endl << "********************************" << std::endl;
+
+    gb::Algorithm::quickSelectMost<int>(intArray, 0, count - 1, mostCount);
+
+    for(int i = 0; i < count; i++)
+    {
+	std::cout << intArray[i] << ", ";
+	if(i == (mostCount - 1))
+	    std::cout << "****, ";
+    }
+
+    std::cout<< std::endl << "********************************" << std::endl;
+
     return 0;
 }
